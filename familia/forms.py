9dbb2410d4ab8082ef496a4beb895a4d6612edb3
pacho.galan <pacho.galan@gmail.com>
2022-05-26 14:26:13 -0500

    Primer cargue

---
+++ familia/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+class PersonaForm(forms.Form):
+    nombre = forms.CharField(label="Nombre", max_length=100)
+    apellido = forms.CharField(label="Apellido", max_length=100)
+    email = forms.EmailField(label="Email")
+    # input_format hace que se pueda ingresar la fecha con el formato latino, dia/mes/año
+    fecha_nacimiento = forms.DateField(label="fecha_nacimiento", input_formats=["%d/%m/%Y"],
+    #widget es para poder agregar un tip para que el usuario sepa como ingresar la fecha
+    widget=forms.TextInput(attrs={'placeholder': '30/12/1995'}))
+    altura = forms.FloatField(widget=forms.NumberInput(attrs={'placeholder': "1.75 m"}))
+
+
+class Persona_PapaForm(forms.Form):
+    nombre = forms.CharField(label="Nombre", max_length=100)
+    apellido = forms.CharField(label="Apellido", max_length=100)
+    #email = forms.EmailField(label="Email")
+    # input_format hace que se pueda ingresar la fecha con el formato latino, dia/mes/año
+    fecha_nacimiento = forms.DateField(label="fecha_nacimiento", input_formats=["%d/%m/%Y"],
+    #widget es para poder agregar un tip para que el usuario sepa como ingresar la fecha
+    widget=forms.TextInput(attrs={'placeholder': '30/12/1995'}))
+    altura = forms.FloatField(widget=forms.NumberInput(attrs={'placeholder': "1.75 m"}))
+
+
+class Persona_MamaForm(forms.Form):
+    nombre = forms.CharField(label="Nombre", max_length=100)
+    apellido = forms.CharField(label="Apellido", max_length=100)
+    #email = forms.EmailField(label="Email")
+    # input_format hace que se pueda ingresar la fecha con el formato latino, dia/mes/año
+    fecha_nacimiento = forms.DateField(label="fecha_nacimiento", input_formats=["%d/%m/%Y"],
+    #widget es para poder agregar un tip para que el usuario sepa como ingresar la fecha
+    widget=forms.TextInput(attrs={'placeholder': '30/12/1995'}))
+    altura = forms.FloatField(widget=forms.NumberInput(attrs={'placeholder': "1.75 m"}))
